1b611b95790c4c361f3c696fa61c8c1435af9c73
mohan-1705 <164631881+mohan-1705@users.noreply.github.com>
2024-10-29 22:38:20 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,11 @@
+function addToCart(product, price) {
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+    cart.push({ product, price });
+    localStorage.setItem("cart", JSON.stringify(cart));
+    alert(`${product} added to cart!`);
+}
+
+function buyNow(product, price) {
+    addToCart(product, price);
+    window.location.href = "cart.html";
+}
